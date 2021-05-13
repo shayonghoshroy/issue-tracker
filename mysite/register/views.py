dfe942a6434968c0a5d1731c0a2bcbf02e230097
Shayon Ghoshroy <sghoshroy21@gmail.com>
2021-05-13 15:37:14 -0400

    created register.html

--- mysite/register/views.py
+++ mysite/register/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 def register(response):
